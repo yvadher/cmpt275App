564ab7fc9397f578fd95a07a9e6d225505feb776
Ryan <ryan@node-1w7jr9qhp4c083ijm67cjei20.ipv6.telus.net>
2017-10-27 04:17:31 -0700

    Part4/15

--- goTalk/goTalk/Controller/ViewController.swift
+++ goTalk/goTalk/Controller/ViewController.swift
@@ -15,8 +15,10 @@ class ViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //  Commentred by Ryan on 2017-10-26.
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout",style: .plain, target: self, action: #selector(handleLogout))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout",style: .plain, target: self, action: #selector(handleLogout))//this is on te viewcontroller page and the button that takes to login page
         
+        view.addSubview(backgroundImageView)
+        setupBackgroundImageView()
     }
     @objc func handleLogout(){
         let loginController = LoginController()
@@ -27,6 +29,25 @@ class ViewController: UITableViewController {
     func preferredStatusBarStyle() -> UIStatusBarStyle{
         return.lightContent
     }
+    //  Commented out by Ryan on 2017-10-26.
+    let backgroundImageView: UIImageView = {
+        let backgroundimageView = UIImageView()
+        backgroundimageView.image = UIImage(named: "goTalk")
+        backgroundimageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundimageView.contentMode = .scaleAspectFill
+        
+        return backgroundimageView
+    }()
+    
+    //  Commented out by Ryan on 2017-10-26.
+    func setupBackgroundImageView(){
+        backgroundImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive=true
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive=true
+        backgroundImageView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: 0).isActive=true
+        backgroundImageView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive=true
+        
+    }
+    
     
     //  Commented out by Ryan on 2017-10-26.
     
