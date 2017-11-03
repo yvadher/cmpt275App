a9a7f9762464622bb0972289274bc19454d9abde
Yagnik Vadher <yvadher@asb9700-e05.csil.sfu.ca>
2017-11-03 00:46:30 -0700

    Added Images and rendered on screen

--- test/Controller/ViewController.swift
+++ test/Controller/ViewController.swift
@@ -13,10 +13,14 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     var photoCategory: [PhotoCategory] = PhotoCategory.fetchPhotos()
     @IBOutlet weak var displayCollection: UICollectionView!
     @IBOutlet weak var categoriesCollection: UICollectionView!
-    @IBOutlet weak var pictographCollection: UICollectionView!
+    @IBOutlet weak var pictographCollection: UICollectionView!    
+    
+    var clickedPhotos: [String] = []
     
     struct Storyboard {
         static let categoryCell = "categoryCell"
+        static let displayBarCell = "displayBarCell"
+        static let pictographCell = "pictographDisplayCell"
         
         static let headerView = "HeaderView"
         
@@ -38,12 +42,18 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
     }
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return photoCategory.count
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if (collectionView == self.categoriesCollection){
-            return 1
+            return photoCategory.count
         }else if (collectionView == self.pictographCollection){
-            return 1
-        }else {
+            return photoCategory[section].imageNames.count
+        }else if (collectionView == self.displayCollection) {
+            return clickedPhotos.count
+        }else{
             return 0
         }
     }
@@ -52,20 +62,35 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         if (collectionView == self.categoriesCollection){
             
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.categoryCell, for: indexPath) as! categoryCell
-            let photoCategory = self.photoCategory[indexPath.item]
-            let categoryImageName =
+            let photos = photoCategory[indexPath.section]
+            let imageName = photos.categoryName
             
-            cell.imageName = imageName
-            
+            //cell.imageName = imageName
+            cell.categoryLable.text = imageName
+            return cell
             
         }else if (collectionView == self.pictographCollection){
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.pictographCell, for: indexPath) as! pictographDisplayCell
+            let photos = photoCategory[indexPath.section]
+            let imageNames = photos.imageNames
+            let imageName = imageNames[indexPath.item]
+            cell.imageName = imageName
+            return cell
             
+        }else if (collectionView == self.displayCollection){
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.displayBarCell, for: indexPath) as! displayBarCell
+            let imageName = clickedPhotos[indexPath.item]
+            
+            cell.imageName = imageName
+            return cell
+            
+        }else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.categoryCell, for: indexPath) as! UICollectionViewCell
+            print ("Came in else cell !! Shoudl not happen!!")
+            return cell
         }
+        
     }
-    
-    
-    
-
  
 
 }
---
+++ test/Model/displayBar.swift
@@ -0,0 +1,13 @@
+//
+//  displayBar.swift
+//  test
+//
+//  Created by Yagnik Vadher on 11/3/17.
+//  Copyright © 2017 ksd8. All rights reserved.
+//
+
+import Foundation
+
+class displayBar {
+    var clickedPhotos: [String] = []
+}
