9e182ef86c9a6b4fb42c8a09e3f2bb9da4858101
Yagnik Vadher <yvadher@asb9700-e05.csil.sfu.ca>
2017-11-03 04:20:50 -0700

    Project files configuration changes

---
+++ test/Controller/LoginController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginController.swift
+//  test
+//
+//  Created by Yagnik Vadher on 11/3/17.
+//  Copyright © 2017 ksd8. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController{
+
+    
+    
+}
