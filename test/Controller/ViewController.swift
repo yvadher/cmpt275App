020af1dd9d5ab0ade81417bfe2f31c65a87a856e
Yagnik Vadher <yvadher@asb9700-e05.csil.sfu.ca>
2017-11-03 02:24:21 -0700

    Added a dynemic category changing

--- test/Controller/ViewController.swift
+++ test/Controller/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  test
 //
-//  Created by ksd8 on 10/28/17.
-//  Copyright © 2017 ksd8. All rights reserved.
+//  Created by yvadher on 10/28/17.
+//  Copyright © 2017 yvadher. All rights reserved.
 //
 
 import UIKit
@@ -16,12 +16,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var pictographCollection: UICollectionView!    
     
     var clickedPhotos: [String] = []
+    var currentCategory: Int = 0
     
     struct Storyboard {
         static let categoryCell = "categoryCell"
         static let displayBarCell = "displayBarCell"
         static let pictographCell = "pictographDisplayCell"
-        
         static let headerView = "HeaderView"
         
         static let leftAndRightPaddings: CGFloat = 2.0
@@ -43,14 +43,26 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return photoCategory.count
+        if (collectionView == self.displayCollection){
+            return 1
+        }else if (collectionView == self.categoriesCollection){
+            return photoCategory.count
+        }else if (collectionView == self.pictographCollection){
+            return 1
+        }else {
+            print ("Should not come here ERROR in numberofsections")
+            return 0
+        }
+        
+       
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if (collectionView == self.categoriesCollection){
-            return photoCategory.count
+            //print (photoCategory.count)
+            return 1
         }else if (collectionView == self.pictographCollection){
-            return photoCategory[section].imageNames.count
+            return photoCategory[currentCategory].imageNames.count
         }else if (collectionView == self.displayCollection) {
             return clickedPhotos.count
         }else{
@@ -71,10 +83,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             
         }else if (collectionView == self.pictographCollection){
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.pictographCell, for: indexPath) as! pictographDisplayCell
-            let photos = photoCategory[indexPath.section]
+            let photos = photoCategory[currentCategory]
             let imageNames = photos.imageNames
             let imageName = imageNames[indexPath.item]
             cell.imageName = imageName
+            print ("Current : \(currentCategory) And : \(photoCategory[indexPath.section].categoryName) ")
+            
             return cell
             
         }else if (collectionView == self.displayCollection){
@@ -85,13 +99,41 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             return cell
             
         }else {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.categoryCell, for: indexPath) as! UICollectionViewCell
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.categoryCell, for: indexPath)
             print ("Came in else cell !! Shoudl not happen!!")
             return cell
         }
         
     }
- 
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if (collectionView == self.categoriesCollection){
+           // let category = self.photoCategory[indexPath.section]
+           // let image = UIImage(named: category.imageNames[indexPath.item])
+            currentCategory = indexPath.section
+            print ("\(currentCategory) : \(indexPath.section) ")
+            
+            pictographCollection.reloadData()
+            
+        }else if (collectionView == self.pictographCollection){
+            
+            let category = self.photoCategory[indexPath.section]
+            clickedPhotos.append(category.imageNames[indexPath.item])
+            print (clickedPhotos)
+            print ("Came here!! \(category.imageNames[indexPath.item])")
+            displayCollection.reloadData()
+           
+            
+        }else if (collectionView == self.displayCollection){
+            //do nothing
+        }else {
+            //Encapsulate in try and catch error block
+            print ("This should not happen ! In the collectionView didselct!")
+        }
+        
+    }
+    
+    
 
 }
 
