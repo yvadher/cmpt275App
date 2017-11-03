f4bdc1311d7e575682f2255901dc18ff659592f1
Yagnik Vadher <yvadher@lib3000-icmac15.ucslabs.sfu.ca>
2017-11-02 22:41:59 -0700

    Added controllers functionality

--- test/Controller/ViewController.swift
+++ test/Controller/ViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-
+    
     var photoCategory: [PhotoCategory] = PhotoCategory.fetchPhotos()
-    
-    
     @IBOutlet weak var displayCollection: UICollectionView!
-    
     @IBOutlet weak var categoriesCollection: UICollectionView!
+    @IBOutlet weak var pictographCollection: UICollectionView!
     
-    @IBOutlet weak var pictographCollection: pictographDisplayCell!
+    struct Storyboard {
+        static let categoryCell = "categoryCell"
+        
+        static let headerView = "HeaderView"
+        
+        static let leftAndRightPaddings: CGFloat = 2.0
+        static let numberOfItemsPerRow: CGFloat = 3.0
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +37,32 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         pictographCollection.dataSource = self
         
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if (collectionView == self.categoriesCollection){
+            return 1
+        }else if (collectionView == self.pictographCollection){
+            return 1
+        }else {
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if (collectionView == self.categoriesCollection){
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.categoryCell, for: indexPath) as! categoryCell
+            let photoCategory = self.photoCategory[indexPath.item]
+            let categoryImageName =
+            
+            cell.imageName = imageName
+            
+            
+        }else if (collectionView == self.pictographCollection){
+            
+        }
+    }
+    
     
     
 
