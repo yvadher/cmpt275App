f7962f893b992a14b14535efaed2be592a7513da
Shawn <shawnt@sfu.ca>
2017-11-10 17:03:10 -0800

    Add files via upload

---
+++ goTalk/Controller/SendingEmailController.swift
@@ -0,0 +1,22 @@
+//
+//  SendingEmailController.swift
+//  Controls the 2nd Password Recovery page talking about sending an email.
+//
+//  CMPT 275 Fall 2017 - Group 02: The Night Owls
+//  Created by Shawn Thai on 11/10/17.
+//  Worked on by Shawn Thai and Yagnik Vadher.
+//
+//  Copyright © 2017 The Night Owls. All rights reserved.
+//
+
+import UIKit
+
+class SendingEmailController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+
+    @IBAction func goToLogin(_ sender: Any) {
+        performSegue(withIdentifier: "toLoginRegisterSegue", sender: self)
+    }
+    
+}
