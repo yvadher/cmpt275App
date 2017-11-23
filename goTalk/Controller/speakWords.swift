97c34520344e9fa2bca4a70060615eaeeb9c3ea6
Yagnik Vadher <yvadher@wmc2502-mac11.ucslabs.sfu.ca>
2017-11-22 18:01:31 -0800

    added framework for the GrammerCorrection

--- goTalk/Controller/speakWords.swift
+++ goTalk/Controller/speakWords.swift
@@ -18,7 +18,61 @@ extension ViewController {
         speechUtterance.rate = 0.45
         speechSynthesizer.speak(speechUtterance)
     }
-
+    
+    // this is how you can join the string
+    //let lineToSpeak: String = clickedPhotos.joined(separator: " ")
+    
+    
+    func correctGrammer(_ sendJSON: String) {
+        
+        //URL for the server API
+        var testFlag = true
+        guard let url = URL(string: "http://textgears.com/api")
+            else {
+                print("Error: URL not found.")
+                return
+        }
+        var request = URLRequest(url: url)
+        
+        //Make a post request object
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: sendJSON, options: [])
+            else {
+                print("Error: Problem with JSON data.")
+                return
+        }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        
+        //Start session to send post request
+        session.dataTask(with: request) { (data, response, error) in
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    print(json)
+                    
+                    let item = json["result"] as! String
+                    //You can get items like item[0].bad...
+                    //MARK: Form a correct string based on the result recived from the api
+                    
+                    
+                    
+                    
+                    //Once done getting string we will call it a speakFunction
+                    //End
+                } catch {
+                    print(error)
+                    testFlag = false
+                }
+            }
+            
+            }.resume()
+    }
+    
 }
 
 
