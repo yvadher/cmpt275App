fca87657544e0bd29fc8c30d5ed8de945a6c8392
ksd8 <ksd8@lib3000-icmac01.ucslabs.sfu.ca>
2017-12-01 19:30:46 -0800

    Set up scrolling pages to be used for tutorial pages. Pages need to be populated with images/info. Need to integrate properly with project. Start view controller has been changed.

---
+++ goTalk/Controller/TutRootPageViewController.swift
@@ -0,0 +1,59 @@
+//
+//  TutRootPageViewController.swift
+//  goTalk
+//
+//  Created by ksd8 on 12/1/17.
+//  Copyright © 2017 The Night Owls. All rights reserved.
+//
+
+import UIKit
+
+class TutRootPageViewController: UIPageViewController, UIPageViewControllerDataSource {
+
+    lazy var viewControllerList:[UIViewController] = {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        
+        let vc1 = sb.instantiateViewController(withIdentifier: "TutOne")
+        let vc2 = sb.instantiateViewController(withIdentifier: "TutTwo")
+        let vc3 = sb.instantiateViewController(withIdentifier: "TutThree")
+        
+        return [vc1, vc2, vc3]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.dataSource = self
+      
+        if let firstViewController = viewControllerList.first{
+            self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+        
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let previousIndex = vcIndex - 1
+        
+        guard previousIndex >= 0 else {return nil}
+        
+        guard viewControllerList.count > previousIndex else {return nil}
+        
+        return viewControllerList[previousIndex]
+        
+        }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let nextIndex = vcIndex + 1
+        
+        guard viewControllerList.count != nextIndex else {return nil}
+        
+        guard viewControllerList.count > nextIndex else {return nil}
+        
+        return viewControllerList[nextIndex]
+    }
+}
